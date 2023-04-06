c694635561ccb59e7e7df1733e6591e66a6cf65d
parkfore <parkfore@naver.com>
2023-04-06 18:57:47 +0900

    nodemon 적용

--- app/app.js
+++ app/app.js
@@ -11,6 +11,6 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 
-app.use("/",home); //use -> 미들 웨이를 등록해주는 메서드
+app.use("/",home); //use -> 미들 웨어를 등록해주는 메서드
 
 module.exports = app;
