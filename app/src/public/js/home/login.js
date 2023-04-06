204cee5a6d528030e98750d8083b8410f3f18813
parkfore <parkfore@naver.com>
2023-04-07 08:29:11 +0900

    백앤드 로그인 구현구

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -10,6 +10,14 @@ function login() {
     const req = {
         id: id.value,
         psword: psword.value
-    }
-    console.log(req)
+    };
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req),
+    }).then((res)=>res.json())
+    .then((res)=>console.log(res));
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,13 +1,42 @@
 "use strict";
+const output = {
+    home : (req, res) => {
+        res.render("home/index");
+     },
+    login: (req, res) => {
+        res.render("home/login");
+    }    
+};
 
-const home = (req, res) => {
-    res.render("home/index");
- };
-const login = (req, res) => {
-    res.render("home/login");
+const user = {
+    id: ["a","b","c"],
+    psword: ["1","2","3"]
+};
+
+const process = {   
+    login: (req, res) => {
+        const id = req.body.id;
+        const psword = req.body.psword;
+        console.log(req.body); 
+        if(user.id.includes(id)){
+            const idx = user.id.indexOf(id);
+            if(psword === user.psword[idx]){
+                return res.json({
+                    success: true
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하였습니다"
+        });
+
+    },
+       
 };
 
 module.exports = {
-    home,
-    login
+    output,
+    process
 }; 
