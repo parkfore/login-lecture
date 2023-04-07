7efdf2af2bdaff1d72b56d9119cdb3c1d2ffc245
parkfore <parkfore@naver.com>
2023-04-07 15:13:13 +0900

    User 모듈 만들기

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -3,7 +3,8 @@
 class UserStorage {
     static #user = {
         id: ["a","b","c"],
-        psword: ["1","2","3"]
+        psword: ["1","2","3"],
+        name: ["가","나","다"]
     };
     static getUsers(...fields) {
         const user = this.#user;
@@ -15,6 +16,17 @@ class UserStorage {
         },{});
         return newUsers;
     };
+
+    static getUserInfo(id) {
+        const user = this.#user;
+        const userIdx = user.id.indexOf(id);
+        const userKeys = Object.keys(user);
+        const userInfo = userKeys.reduce((userInfo,keys) => {
+            userInfo[keys] = user[keys][userIdx];
+            return userInfo;
+        },{});
+        return userInfo;
+    }
 }
 
 module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     home : (req, res) => {
@@ -13,19 +13,8 @@ const output = {
 
 const process = {   
     login: (req, res) => {
-        const id = req.body.id;
-        const psword = req.body.psword;
-        const user = UserStorage.getUsers("id","psword");
-        const response = {};
-        if(user.id.includes(id)){
-            const idx = user.id.indexOf(id);
-            if(psword === user.psword[idx]){
-                response.success = true;
-                return res.json(response);
-            }
-        }
-        response.success = false;
-        response.msg = "로그인에 실패하였습니다.";
+        const user = new User(req.body);
+        const response = user.login()
         return res.json(response);
     },
        
