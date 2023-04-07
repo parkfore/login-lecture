dfa310c3b858ea95ac58f2150fa20abc4ef0f5be
parkfore <parkfore@naver.com>
2023-04-07 16:40:30 +0900

    회원가입 화면 만들기&꾸미기

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -6,8 +6,11 @@ const output = {
     home : (req, res) => {
         res.render("home/index");
      },
-    login: (req, res) => {
+     login: (req, res) => {
         res.render("home/login");
+    },   
+    register: (req, res) => {
+        res.render("home/register");
     }    
 };
 
